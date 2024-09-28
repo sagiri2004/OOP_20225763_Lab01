6d176ec259d4efd29ba64db199cdbc31eccbf0f9
sagiri2004 <thang.nh225763@sis.hust.edu.vn>
2024-09-28 11:24:39 +0700

    first commit

---
+++ FirstDialog.java
@@ -0,0 +1,11 @@
+// FirstDialog.java
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+        // exit 
+        System.exit(0);
+    }
+}
